e39e8aff2e6775c2916ea1cefe4ca6c2b01506e9
Nick Cross <ncross@redhat.com>
2014-05-21 12:58:35 +0100

    Improve documentation. Refactor versionOverride to two supported formats. Only apply depMgmt and pluginMgmt to top level POM. Add dependencies at start of depMgmt rather than end

--- src/main/java/org/commonjava/maven/ext/manip/state/BOMState.java
+++ src/main/java/org/commonjava/maven/ext/manip/state/BOMState.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- * 
+ *
  * Contributors:
  *     Red Hat, Inc. - initial API and implementation
  ******************************************************************************/
@@ -34,6 +34,18 @@ public class BOMState
      * <code>-DdependencyExclusion.junit:junit@org.groupId:artifactId</code>
      */
     public static final String DEPENDENCY_EXCLUSION_PREFIX = "dependencyExclusion.";
+
+    /**
+     * Two possible formats currently supported for version property output:</br>
+     * <code>version.group</code></br>
+     * <code>version.group.artifact</code></br>
+     * Configured by the property <code>-DversionPropertyFormat=[VG|VGA]</code>
+     */
+    public static enum VersionPropertyFormat
+    {
+        VG,
+        VGA;
+    };
 
     /**
      * Suffix to enable this modder. The name of the property which contains the GAV of the remote pom from
