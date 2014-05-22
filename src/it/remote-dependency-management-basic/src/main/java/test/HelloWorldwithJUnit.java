956556ac532ccf22acf03d1819e6142515670bb8
Darren Coleman <dcoleman@localhost.localdomain>
2014-05-22 15:24:29 +0100

    Added remote-dependency-management-basic test.

---
+++ src/it/remote-dependency-management-basic/src/main/java/test/HelloWorldwithJUnit.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.Test;
+
+public class HelloWorldwithJUnit
+{
+    public static void main (String [] args)
+    {
+        System.out.println("hello");
+    }
+
+    @Test
+    public void test()
+    {
+        // Just a dummy method to verify that we can compile again JUnit 4
+    }
+}
