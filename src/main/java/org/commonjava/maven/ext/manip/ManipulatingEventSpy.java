0a1b81b7e2932063580656744911795fc5b5cfc3
John Casey <jdcasey@commonjava.org>
2014-06-16 11:07:46 -0500

    Adding diagnostics to message when extension is disabled.

--- src/main/java/org/commonjava/maven/ext/manip/ManipulatingEventSpy.java
+++ src/main/java/org/commonjava/maven/ext/manip/ManipulatingEventSpy.java
@@ -84,7 +84,10 @@ public class ManipulatingEventSpy
 
                         if ( !session.isEnabled() )
                         {
-                            logger.info( "Manipulation engine disabled." );
+                            logger.info( "Manipulation engine disabled {}",
+                                         ( session.getTopPom() == null ? ". No project found."
+                                                         : " via command-line option" ) );
+
                             super.onEvent( event );
                             return;
                         }
