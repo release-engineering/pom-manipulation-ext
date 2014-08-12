cfc37437753560d096c6cd3ab7e9185919cf8e4e
Nick Cross <ncross@redhat.com>
2014-08-12 23:27:21 +0100

    Wildcard test fixes

--- src/it/depex-wildcard/verify.groovy
+++ src/it/depex-wildcard/verify.groovy
@@ -5,13 +5,13 @@ def pom = new XmlSlurper().parse( pomFile )
 
 def dependency = pom.dependencyManagement.dependencies.dependency.find { it.artifactId.text() == "junit" }
 assert dependency != null
-assert dependency.version.text() == "4.10"
+assert dependency.version.text() != "4.11"
 
-pomFile = new File( basedir, 'child2/pom.xml' )
+pomFile = new File( basedir, 'child1/pom.xml' )
 System.out.println( "Slurping POM: ${pomFile.getAbsolutePath()}" )
 
 pom = new XmlSlurper().parse( pomFile )
 
 dependency = pom.dependencyManagement.dependencies.dependency.find { it.artifactId.text() == "junit" }
 assert dependency != null
-assert dependency.version.text() == "4.11"
+assert dependency.version.text() != "4.11"
