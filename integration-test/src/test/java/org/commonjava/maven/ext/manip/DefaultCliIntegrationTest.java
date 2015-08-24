5827c724ae62eff6c3a518ff9eaa33dbdca899a0
Nick Cross <ncross@redhat.com>
2015-08-24 22:48:07 +0100

    Add test to detect test suite being run with Maven with PME installed. Add shortcut to run single test under CLI mode

--- integration-test/src/test/java/org/commonjava/maven/ext/manip/DefaultCliIntegrationTest.java
+++ integration-test/src/test/java/org/commonjava/maven/ext/manip/DefaultCliIntegrationTest.java
@@ -17,7 +17,9 @@
 package org.commonjava.maven.ext.manip;
 
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
@@ -41,12 +43,21 @@ public class DefaultCliIntegrationTest {
     @Parameters(name="{0}")
     public static Collection<Object[]> getFiles() {
         Collection<Object[]> params = new ArrayList<Object[]>();
-        for (File rl : new File(IT_LOCATION).listFiles()) {
-            if (rl.isDirectory() && !EXCLUDED_FILES.contains(rl.getName())) {
-                Object[] arr = new Object[] { rl.getName() };
-                params.add(arr);
+        // Hack to allow a single parameterized test to be run.
+        if ( System.getProperties().containsKey("cli-test"))
+        {
+            params.add (new Object[] { System.getProperty("cli-test") } );
+        }
+        else
+        {
+            for (File rl : new File(IT_LOCATION).listFiles()) {
+                if (rl.isDirectory() && !EXCLUDED_FILES.contains(rl.getName())) {
+                    Object[] arr = new Object[] { rl.getName() };
+                    params.add(arr);
+                }
             }
         }
+
         return params;
     }
 
