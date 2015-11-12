c83e946ee438374cccbb74a24fae7b4abad19b40
John Casey <jdcasey@commonjava.org>
2015-11-12 17:05:50 -0600

    Use system properties to figure out what JVM is in use, so CI system builds will fork the right JVM for integration tests.

--- integration-test/src/test/java/org/commonjava/maven/ext/manip/RESTIntegrationTest.java
+++ integration-test/src/test/java/org/commonjava/maven/ext/manip/RESTIntegrationTest.java
@@ -19,6 +19,8 @@ import org.commonjava.maven.ext.manip.rest.rule.MockServer;
 import org.junit.ClassRule;
 import org.junit.Test;
 
+import java.io.File;
+
 import static org.commonjava.maven.ext.manip.CliTestUtils.getDefaultTestLocation;
 import static org.commonjava.maven.ext.manip.CliTestUtils.runLikeInvoker;
 
