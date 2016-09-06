e6388ad17da72f48f8613515b698033d1835d86c
Nick Cross <ncross@redhat.com>
2016-09-06 17:08:39 +0200

    Licensing update

--- core/src/test/java/org/commonjava/maven/ext/manip/fixture/PlexusTestRunner.java
+++ core/src/test/java/org/commonjava/maven/ext/manip/fixture/PlexusTestRunner.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (C) 2012 Red Hat, Inc. (jcasey@redhat.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
--- io/src/main/java/org/commonjava/maven/ext/manip/rest/mapper/DAMapper.java
+++ io/src/main/java/org/commonjava/maven/ext/manip/rest/mapper/DAMapper.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (C) 2012 Red Hat, Inc. (jcasey@redhat.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.commonjava.maven.ext.manip.rest.mapper;
 
 import java.util.List;
