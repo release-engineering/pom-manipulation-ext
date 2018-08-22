9e3fd2c8f428611fe032ad6b74922664b411fdea
Nick Cross <ncross@redhat.com>
2018-08-22 15:14:16 +0100

    NCL-4022 Add parent injection

---
+++ core/src/main/java/org/commonjava/maven/ext/core/state/ParentInjectionState.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2012 Red Hat, Inc. (jcasey@redhat.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.commonjava.maven.ext.core.state;
+
+import org.apache.maven.model.Parent;
+import org.commonjava.maven.atlas.ident.ref.InvalidRefException;
+import org.commonjava.maven.atlas.ident.ref.ProjectVersionRef;
+import org.commonjava.maven.atlas.ident.ref.SimpleProjectVersionRef;
+import org.commonjava.maven.ext.core.impl.RepositoryInjectionManipulator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+/**
+ * Captures configuration relating to injection of parent.
+ * Used by {@link RepositoryInjectionManipulator}.
+ */
+public class ParentInjectionState
+    implements State
+{
+    private final Logger logger = LoggerFactory.getLogger( getClass() );
+
+    /**
+     * Suffix to enable this modder
+     */
+    private static final String PARENT_INJECTION_PROPERTY = "parentInjection";
+
+    private final Parent parent;
+
+    public ParentInjectionState( final Properties userProps )
+    {
+        final String gav = userProps.getProperty( PARENT_INJECTION_PROPERTY );
+
+        if ( gav != null )
+        {
+            ProjectVersionRef ref;
+            try
+            {
+                ref = SimpleProjectVersionRef.parse( gav );
+            }
+            catch ( final InvalidRefException e )
+            {
+                logger.error( "Skipping parent injection! Got invalid GAV: {}", gav );
+                throw e;
+            }
+            parent = new Parent();
+            parent.setGroupId( ref.getGroupId() );
+            parent.setArtifactId( ref.getArtifactId() );
+            parent.setVersion( ref.getVersionString() );
+            parent.setRelativePath( "" );
+        }
+        else
+        {
+            parent = null;
+        }
+    }
+
+    /**
+     * Enabled ONLY if parentInjection is provided in the user properties / CLI -D options.
+     *
+     * @see #PARENT_INJECTION_PROPERTY
+     * @see State#isEnabled()
+     */
+    @Override
+    public boolean isEnabled()
+    {
+        return parent != null;
+    }
+
+    public Parent getParentInjection()
+    {
+        return parent;
+    }
+}
+
---
+++ integration-test/src/it/parent-injection-basic/verify.groovy
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C) 2012 Red Hat, Inc. (jcasey@redhat.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+def pomFile = new File( basedir, 'pom.xml' )
+System.out.println( "Slurping POM: ${pomFile.getAbsolutePath()}" )
+
+def pom = new XmlSlurper().parse( pomFile )
+def parent = pom.parent
+
+assert parent.list().size() > 0
+
+def v = parent.version.text()
+def g = parent.groupId.text()
+def a = parent.artifactId.text()
+
+assert g.equals ("org.jboss")
+assert a.equals ("jboss-parent")
+assert v.equals ("11")
