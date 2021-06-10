8cb1f5d853194bec9765b141eb4dca702ac21564
Nick Cross <ncross@redhat.com>
2021-06-10 07:35:00 +0100

    NCLSUP365 Scan all pom files even those with variables in groupId/art… (#838)

---
+++ core/src/test/java/org/commonjava/maven/ext/core/util/PomIOTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2012 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.commonjava.maven.ext.core.util;
+
+import org.apache.commons.io.FileUtils;
+import org.commonjava.maven.ext.common.model.Project;
+import org.commonjava.maven.ext.core.fixture.TestUtils;
+import org.commonjava.maven.ext.io.PomIO;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.util.List;
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class PomIOTest
+{
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog().muteForSuccessfulTests();
+
+    @Test
+    public void testVerifyParsePomTemplatesDefault() throws Exception
+    {
+        final File projectroot = folder.newFile();
+        final File resource = TestUtils.resolveFileResource( "", "pom-variables.xml" );
+        assertNotNull( resource );
+        FileUtils.copyFile( resource, projectroot );
+
+        PomIO pomIO = new PomIO(TestUtils.createSessionAndManager( new Properties() ).getSession() );
+        List<Project> projects = pomIO.parseProject( projectroot );
+        assertEquals( 1, projects.size() );
+        assertEquals( "${one}", projects.get( 0 ).getGroupId() );
+    }
+
+    @Test
+    public void testVerifyParsePomTemplatesFalse() throws Exception
+    {
+        final File projectroot = folder.newFile();
+        final File resource = TestUtils.resolveFileResource( "", "pom-variables.xml" );
+        assertNotNull( resource );
+        FileUtils.copyFile( resource, projectroot );
+
+        Properties p = new Properties();
+        p.put( PomIO.PARSE_POM_TEMPLATES, "false" );
+        PomIO pomIO = new PomIO(TestUtils.createSessionAndManager( p ).getSession() );
+        List<Project> projects = pomIO.parseProject( projectroot );
+        assertEquals( 0, projects.size() );
+        assertTrue( systemOutRule.getLog().contains( "PomPeek - Could not peek at POM coordinate for" ) );
+    }
+}
