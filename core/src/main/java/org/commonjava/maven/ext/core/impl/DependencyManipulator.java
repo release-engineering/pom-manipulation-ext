56c3b16213e27a70663792cf95291b95a62d0620
David Walluck <dwalluck@redhat.com>
2021-11-21 16:49:19 -0500

    Fix log message

--- core/src/main/java/org/commonjava/maven/ext/core/impl/DependencyManipulator.java
+++ core/src/main/java/org/commonjava/maven/ext/core/impl/DependencyManipulator.java
@@ -649,7 +649,8 @@ public class DependencyManipulator extends CommonManipulator implements Manipula
                 if ( e.getKey().getGroupId().equals( p.getGroupId() )
                         && e.getKey().getArtifactId().equals( p.getArtifactId() ) )
                 {
-                    logger.warn( "Removing version override for {} since it matches project", e.getKey() );
+                    logger.warn( "Removing version override for {}:{} since it matches project",
+                            e.getKey().getGroupId(), e.getKey().getArtifactId() );
                     it.remove();
                 }
             } );
