9ed8c5f8920fc0f49a25deead6a5f0281bb7b333
jmichalo <jmichalo@redhat.com>
2023-05-09 12:29:01 +0200

    fix checkDependencies() test

--- cli/src/test/java/org/commonjava/maven/ext/cli/CliTest.java
+++ cli/src/test/java/org/commonjava/maven/ext/cli/CliTest.java
@@ -360,7 +360,7 @@ public class CliTest
                 + "org.hamcrest:hamcrest-all:1.3                                                   jar                                     test                \n"
                 + "org.jacoco:jacoco-maven-plugin:0.8[.\\d+]+\\s+                                  maven-plugin                                                \n"
                 + "org.jboss.byteman:byteman-bmunit:4[.\\d+]+\\s+                                  jar                                     test                \n"
-                + "org.jboss.da:reports-model:2.[.\\d+]+\\s+                                       jar                                     compile             \n"
+                + "org.jboss.da:reports-model:2.[.\\d+]+(-SNAPSHOT)?\\s+                           jar                                     compile             \n"
                 + "org.jdom:jdom2:2.[.\\d+]+\\s+                                                   jar                                     compile             \n"
                 + "org.projectlombok:lombok:1.[.\\d+]+\\s+                                         jar                                     provided            \n"
                 + "org.projectlombok:lombok-maven-plugin:1.[.\\d+]+\\s+                            maven-plugin                                                \n"
